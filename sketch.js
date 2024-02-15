af0ed5f2b8670812ecbdfe88924059d2265f5b47
nocassage <121447847+nocassage@users.noreply.github.com>
2024-02-15 15:31:27 +0000

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,41 @@
+let video;
+let handPose;
+let hands;
+let vg = 57;
+let vgs = 0;
+
+function modelReady() {
+  console.log("hand pose loaded");
+  handpose.on("predict", gotPose);
+}
+
+function mousePressed() {
+  console.log(hands);
+}
+
+function gotPose(results) {
+  hands = results;
+}
+
+function setup() {
+  createCanvas(400, 500, WEBGL);
+  video = createCapture(VIDEO);
+  video.hide();
+  handpose = ml5.handpose(video, modelReady);
+}
+
+function draw() {
+  if (hands && hands.length > 0) {
+    if (vg < 200) vg+=2;
+    if (vgs < 255) vgs+=2;
+  } else {
+    if (vg > 57) vg-=2;
+    if (vgs > 0) vgs-=2;
+  }
+  background(0);
+  fill(88, vg, 59);
+  stroke(0, 0, vgs);
+  rotateY(millis() / 4000);
+  strokeWeight(0.3);
+  ellipsoid(100, 100, 100);
+}
